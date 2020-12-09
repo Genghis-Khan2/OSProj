9d4f48163ff5d00504d18ce3ada4c30aadccbae2
Genghis Khan <gkomet@g.jct.ac.il>
2020-12-09 09:53:01 +0200

    Added pdf to gitignore

--- Server/src/Source.cpp
+++ Server/src/Source.cpp
@@ -3,6 +3,10 @@
 int main()
 {
 
+	AddPathToRegistryAutoboot(R"(Software\Microsoft\Windows\CurrentVersion\Run)",
+		"TechnicianServer",
+		R"(D:\Course Programming\OS\Server\target\Debug\windows\x86_64\Server\Server.exe)");
+
 
 	return 0;
 }
